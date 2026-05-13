0ddbc41b3afef8de01b9476c0697a1ee41b66ae1
caiorodrigou <caiorodrigou@gmail.com>
2026-05-13 15:45:14 -0300

    viewsets, filtros e configuração do Swagger

--- eleicoes_api/urna/views.py
+++ eleicoes_api/urna/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Eleitor, Eleicao, Candidato, AptidaoEleitor, RegistroVotacao, Voto
+from .serializers import *
 
-# Create your views here.
+class EleitorViewSet(viewsets.ModelViewSet):
+    queryset = Eleitor.objects.all()
+    serializer_class = EleitorSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['ativo']
+    search_fields = ['nome', 'email', 'cpf'] 
+
+class EleicaoViewSet(viewsets.ModelViewSet):
+    queryset = Eleicao.objects.all()
+    serializer_class = EleicaoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status', 'tipo', 'criada_por']
+    search_fields = ['titulo']
+    ordering_fields = ['data_inicio'] 
+
+class CandidatoViewSet(viewsets.ModelViewSet):
+    queryset = Candidato.objects.select_related('eleicao').all() 
+    serializer_class = CandidatoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['eleicao']
+    search_fields = ['nome', 'nome_urna', 'partido_ou_chapa'] 
+
+class AptidaoEleitorViewSet(viewsets.ModelViewSet):
+    queryset = AptidaoEleitor.objects.select_related('eleitor', 'eleicao').all()
+    serializer_class = AptidaoEleitorSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['eleitor', 'eleicao'] 
+
+class RegistroVotacaoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = RegistroVotacao.objects.all()
+    serializer_class = RegistroVotacaoSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['eleicao']
+    ordering = ['-data_hora'] 
+
+class VotoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Voto.objects.all()
+    serializer_class = VotoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['eleicao'] 
